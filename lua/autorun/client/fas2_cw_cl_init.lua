f57b0fb844fe15e62904c0a83a3010668a98b175
Xiao <huynhxiaoping@gmail.com>
2021-06-23 22:05:35 -0500

    wip: cleanup

--- lua/autorun/client/fas2_cw_cl_init.lua
+++
@@ -1,3 +0,0 @@
-AddCSLuaFile()
--- stupid file name in order to force it to load after the cw2 base shells file
-include("cw/client/fas2_cw_statdisplay.lua")
--- lua/autorun/fas2_cw_init.lua
+++
@@ -1,4 +0,0 @@
-AddCSLuaFile()
-
--- stupid file name in order to force it to load after the cw2 base shells file
-include("cw/shared/fas2_cw_shells.lua")
